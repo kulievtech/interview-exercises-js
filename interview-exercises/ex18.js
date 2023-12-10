494c4aa820b0af9433fd1834941a5cb1e1772865
Tohir Kuliev <kuliev.tech@gmail.com>
2023-12-10 17:56:10 -0500

    work on array problem:

--- interview-exercises/ex18.js
+++ interview-exercises/ex18.js
@@ -1,5 +1,7 @@
 // Write a JavaScript function that takes an array as input. This array can contain elements that are either numbers or arrays of numbers, but the arrays will not contain further nested arrays. The function should return a new array that is a flattened version of the input array, where all elements are at the root level.
 
+//
+
 // [1,2,3[4,5,6[7,8,9]]]=>[1,2,3,4,5,6]
 const flattenArray = (arr) => {
     let newArr = [];
